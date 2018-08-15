72a0cc3cfad851a26ea5ff4203582704ca66f937
j.o.lantern0422 <j.o.lantern0422@gmail.com>
2018-08-15 13:18:16 +0900

    チュートリアルを終えた

---
+++ popup.js
@@ -0,0 +1,25 @@
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+
+    let entity = {};
+    entity.hoge = {
+        col1: 'new data'
+    };
+
+    chrome.storage.local.set(entity, function() {
+        console.log('stored');
+    });
+  };
